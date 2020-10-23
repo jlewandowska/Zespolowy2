b343abc8f533730c5ab8909ef5bedd5183a0afc9
ssmolinski <slawomirsmolinski1@gmail.com>
2020-10-23 21:58:47 +0200

    Map

---
+++ ProjektZespolowy/Assets/elevator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class elevator : MonoBehaviour
+{
+  public GameObject movePlatform;
+
+  private void OnTriggerStay()
+  {
+    movePlatform.transform.position += movePlatform.transform.up * 10 * Time.deltaTime;
+  }
+}
