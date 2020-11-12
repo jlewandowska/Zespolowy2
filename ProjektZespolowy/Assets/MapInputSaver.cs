2e2a6f0fa818cff69cee2308dd78d43b8fdaf320
Joanna Lewandowska <joannalewandowska0996@gmail,com>
2020-11-12 18:19:38 +0100

    menu & flag cnt

---
+++ ProjektZespolowy/Assets/MapInputSaver.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapInputSaver : MonoBehaviour
+{
+    public GameObject inputField;
+
+    public void saveMapInput()
+    {
+        var obj = inputField.GetComponent<InputField>();
+        var text = obj.text;
+
+        string fileName = "wyniki/flagi.txt";
+        System.IO.Directory.CreateDirectory("wyniki");
+        using (StreamWriter writetext = new StreamWriter(fileName))
+        {
+            
+            writetext.WriteLine("Ilość map: " + text);
+        }
+    }
+}
+
