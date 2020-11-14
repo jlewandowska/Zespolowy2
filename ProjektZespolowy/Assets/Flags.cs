d5c2c03f69a0f28790f8d6c16d2fa783a238b5e7
ssmolinski <slawomirsmolinski1@gmail.com>
2020-11-14 10:38:45 +0100

    Visibility script

---
+++ ProjektZespolowy/Assets/Flags.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flags : MonoBehaviour
+{
+  Renderer m_renderer;
+  GameObject player;
+  string visibilityStatus;
+
+  void Start()
+  {
+    m_renderer = GetComponent<Renderer>();
+    player = GameObject.Find("Player");
+  }
+
+  void FixedUpdate()
+  {
+    if(m_renderer.isVisible)
+    {
+      if (Physics.Linecast(gameObject.transform.position, player.transform.position))
+        visibilityStatus = "Niewidoczne";
+      else
+        visibilityStatus = "Widoczne";
+    }
+    else
+      visibilityStatus = "Niewidoczne";
+  }
+
+  void OnGUI()
+  {
+    GUI.Label(new Rect(10, 10, 1920, 20), visibilityStatus);
+  }
+}
